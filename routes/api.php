319c4220ec1232a9dbd3cef25c25ebffdc790124
RAIMUNDO Paulo <Paulo.RAIMUNDO@essor.com.br>
2023-10-08 20:46:35 -0300

    feat: Implementando rotas de Login e register e Guard JWT.

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('user')->group(function () {
-        Route::post('/create', [\App\Http\Controllers\Api\UserController::class, 'create']);
-        Route::get('/getAllUsers', [\App\Http\Controllers\Api\UserController::class, 'getAllUsers']);
-    });
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
 
-    Route::prefix('transaction')->group(function () {
-        Route::post('/create', [\App\Http\Controllers\Api\TransactionController::class, 'createTransaction']);
+    Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('/register', [\App\Http\Controllers\Api\UserController::class, 'create']);
+    Route::post('/logout', [\App\Http\Controllers\Api\UserController::class, 'logout']);
+});
+
+Route::middleware(['auth:api', 'prefix' => 'jwt.refresh'])->group(function() {
+    Route::prefix('v1')->group(function () {
+        Route::prefix('user')->group(function () {
+            Route::get('/getAllUsers', [\App\Http\Controllers\Api\UserController::class, 'getAllUsers']);
+        });
+
+        Route::prefix('transaction')->group(function () {
+            Route::post('/create', [\App\Http\Controllers\Api\TransactionController::class, 'createTransaction']);
+        });
     });
 });
+
+
